af4ca1e37a834857d2f782c1a9d18e815f46228a
Niko <niko@shiftleft.io>
2021-07-08 12:55:28 +0200

    Refactoring test code

--- src/test/resources/test.c
+++ src/test/resources/test.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
-int test2(char* x){
+#include <string.h>
+void test2(char* x){
   char dest[4096];
   strcpy(dest,x);
 }
+
 int test1(int y) {
-  printf("%i\n", y+20);
+  printf("test1 function %i\n", y+20);
   char xy[] = "abcdefg";
   test2(xy);
   return y+20;
 }
+
 int test(int x) {
-  char y[] = "TEST";
+  char y[] = "test function";
   printf("%s\n", y);
   test1(x);
   return x+(x*2)+20;
 }
+
 int main (int argc, char** argv) {
-  printf("ok\n");
+  printf("main function\n");
   test(20);
   return 0;
 }
