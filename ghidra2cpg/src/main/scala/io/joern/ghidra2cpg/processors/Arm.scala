f9186f170c1204f0a643eb200b299cb221c2cb48
Niko <niko@shiftleft.io>
2021-09-08 10:41:19 +0200

    Instruction fixes

--- ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/processors/Arm.scala
+++ ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/processors/Arm.scala
@@ -1,16 +1,16 @@
 package io.joern.ghidra2cpg.processors
-
 import scala.collection.immutable._
+
 class Arm extends Processor {
   override def getInstructions: HashMap[String, String] =
     HashMap(
-      "add"  -> "<operator>.addition",
+      "add"  -> "<operator>.incBy",
       "adrp" -> "TODO",
       "asr"  -> "TODO",
       "b"    -> "<operator>.goto",
       "b.eq" -> "<operator>.goto",
       "b.ne" -> "<operator>.goto",
-      "bl"   -> "<operator>.goto",
+      "bl"   -> "CALL",
       "blr"  -> "<operator>.goto",
       "br"   -> "<operator>.goto",
       "bti"  -> "<operator>.goto",
@@ -27,9 +27,8 @@ class Arm extends Processor {
       "nop"  -> "<operator>.NOP",
       "ret"  -> "RETURN",
       "stp"  -> "<operator>.assignment",
-      "str"  -> "<operator>.assignment",
+      "str"  -> "<operator>.addition",
       "strb" -> "<operator>.assignment",
       "sub"  -> "<operator>.subtraction"
     )
 }
-
