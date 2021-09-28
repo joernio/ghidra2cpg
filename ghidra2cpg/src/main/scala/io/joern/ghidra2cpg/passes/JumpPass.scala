88a6db9588a9c0bb56630ee5307eb9c89c572c78
Niko <niko@shiftleft.io>
2021-09-28 14:57:30 +0200

    JumpPass

--- ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/JumpPass.scala
+++ ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/JumpPass.scala
@@ -4,11 +4,13 @@ import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.EdgeTypes
 import io.shiftleft.passes.{DiffGraph, IntervalKeyPool, ParallelCpgPass}
 import io.shiftleft.semanticcpg.language._
+
 class JumpPass(cpg: Cpg, keyPool: IntervalKeyPool)
     extends ParallelCpgPass[String](
       cpg,
       keyPools = Some(keyPool.split(1))
     ) {
+
   override def partIterator: Iterator[String] = List("").iterator
 
   override def runOnPart(part: String): Iterator[DiffGraph] = {
@@ -22,7 +24,11 @@ class JumpPass(cpg: Cpg, keyPool: IntervalKeyPool)
             cpg.call.lineNumber(Integer.parseInt(destinationAddress, 16)).foreach { destination =>
               diffGraph.addEdge(sourceCall, destination, EdgeTypes.CFG)
             }
-          case _ => //
+          case _ => // Ignore for now
+          /*
+            TODO:
+              - Ask ghidra to resolve addresses of JMPs
+           */
         }
       }
     Iterator(diffGraph.build())
