58a140b448843fea0ad2dd17cee7f2adae9fb454
Niko <niko@shiftleft.io>
2021-09-28 13:11:10 +0200

    Scalafmt

--- ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/JumpPass.scala
+++ ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/JumpPass.scala
@@ -8,14 +8,15 @@ class JumpPass(cpg: Cpg) extends CpgPass(cpg) {
 
   override def run(): Iterator[DiffGraph] = {
     implicit val diffGraph: DiffGraph.Builder = DiffGraph.newBuilder
-    cpg.call.nameExact("<operator>.goto")
-       .where(_.argument.order(1).isLiteral)
-       .foreach{ sourceCall =>
-         val destinationAddress = sourceCall.argument.order(1).code.l.headOption.getOrElse("")
-         cpg.call.lineNumber(Integer.parseInt(destinationAddress, 16)).foreach { destination=>
-           diffGraph.addEdge(sourceCall, destination, EdgeTypes.CFG)
-         }
-    }
+    cpg.call
+      .nameExact("<operator>.goto")
+      .where(_.argument.order(1).isLiteral)
+      .foreach { sourceCall =>
+        val destinationAddress = sourceCall.argument.order(1).code.l.headOption.getOrElse("")
+        cpg.call.lineNumber(Integer.parseInt(destinationAddress, 16)).foreach { destination =>
+          diffGraph.addEdge(sourceCall, destination, EdgeTypes.CFG)
+        }
+      }
     Iterator(diffGraph.build())
   }
 }
