729f292f8bd397f843eb33763ecef11fb0be5828
Johannes Coetzee <jcoetzee@shiftleft.io>
2021-09-28 17:18:33 +0200

    Only run JumpPass per method

--- ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/JumpPass.scala
+++ ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/JumpPass.scala
@@ -2,26 +2,24 @@ package io.joern.ghidra2cpg.passes
 
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.EdgeTypes
-import io.shiftleft.passes.{DiffGraph, IntervalKeyPool, ParallelCpgPass}
+import io.shiftleft.codepropertygraph.generated.nodes.Method
+import io.shiftleft.passes.{ConcurrentWriterCpgPass, DiffGraph, IntervalKeyPool, ParallelCpgPass}
 import io.shiftleft.semanticcpg.language._
 
-class JumpPass(cpg: Cpg, keyPool: IntervalKeyPool)
-    extends ParallelCpgPass[String](
-      cpg,
-      keyPools = Some(keyPool.split(1))
-    ) {
+class JumpPass(cpg: Cpg)
+    extends ConcurrentWriterCpgPass[Method](cpg) {
 
-  override def partIterator: Iterator[String] = List("").iterator
+  override def generateParts(): Array[Method] = cpg.method.toArray
 
-  override def runOnPart(part: String): Iterator[DiffGraph] = {
+  override def runOnPart(diffGraph: DiffGraph.Builder, method: Method): Unit = {
     implicit val diffGraph: DiffGraph.Builder = DiffGraph.newBuilder
-    cpg.call
+    method.call
       .nameExact("<operator>.goto")
       .where(_.argument.order(1).isLiteral)
       .foreach { sourceCall =>
         sourceCall.argument.order(1).code.l.headOption match {
           case Some(destinationAddress) =>
-            cpg.call.lineNumber(Integer.parseInt(destinationAddress, 16)).foreach { destination =>
+            method.call.lineNumber(Integer.parseInt(destinationAddress, 16)).foreach { destination =>
               diffGraph.addEdge(sourceCall, destination, EdgeTypes.CFG)
             }
           case _ => // Ignore for now
@@ -31,6 +29,5 @@ class JumpPass(cpg: Cpg, keyPool: IntervalKeyPool)
            */
         }
       }
-    Iterator(diffGraph.build())
   }
 }
