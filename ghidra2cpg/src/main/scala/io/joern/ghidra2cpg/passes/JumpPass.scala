97988d004cac6b9e0ee2cd7886ad506dc6df5986
Johannes Coetzee <jcoetzee@shiftleft.io>
2021-09-29 12:25:17 +0200

    Parse address safely in JumpPass

--- ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/JumpPass.scala
+++ ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/JumpPass.scala
@@ -6,6 +6,8 @@ import io.shiftleft.codepropertygraph.generated.nodes.Method
 import io.shiftleft.passes.{DiffGraph, IntervalKeyPool, ParallelCpgPass}
 import io.shiftleft.semanticcpg.language._
 
+import scala.util.Try
+
 class JumpPass(cpg: Cpg, keyPool: IntervalKeyPool)
   extends ParallelCpgPass[Method](
     cpg,
@@ -14,15 +16,19 @@ class JumpPass(cpg: Cpg, keyPool: IntervalKeyPool)
 
   override def partIterator: Iterator[Method] = cpg.method.l.iterator
 
+  private def parseAddress(address: String): Option[Int] = {
+    Try(Integer.parseInt(address, 16)).toOption
+  }
+
   override def runOnPart(method: Method): Iterator[DiffGraph] = {
   implicit val diffGraph: DiffGraph.Builder = DiffGraph.newBuilder
     method.call
       .nameExact("<operator>.goto")
       .where(_.argument.order(1).isLiteral)
       .foreach { sourceCall =>
-        sourceCall.argument.order(1).code.l.headOption match {
+        sourceCall.argument.order(1).code.l.headOption.flatMap(parseAddress) match {
           case Some(destinationAddress) =>
-            method.call.lineNumber(Integer.parseInt(destinationAddress, 16)).foreach {
+            method.call.lineNumber(destinationAddress).foreach {
               destination =>
                 diffGraph.addEdge(sourceCall, destination, EdgeTypes.CFG)
             }
