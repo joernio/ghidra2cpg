9b2fbfe67390f8b5970d625fce7266e9e3b6c5a5
Johannes Coetzee <jcoetzee@shiftleft.io>
2021-09-29 17:49:54 +0200

    Add build instructions to asm file

--- ghidra2cpg-tests/src/test/testbinaries/cfg.asm
+++ ghidra2cpg-tests/src/test/testbinaries/cfg.asm
@@ -1,3 +1,4 @@
+;; Built with `nasm -felf64 cfg.asm -g` and `gcc cfg.o -o cfg.bin`
 global main
 
 section .text
@@ -14,6 +15,7 @@ main:
     add rax, 2
     sub rcx, 1
     jmp .loop
+
 .end:
     mov rsp, rbp
     pop rbp
