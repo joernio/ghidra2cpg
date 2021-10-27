a1cb09b043e26e2b245d7989c779f85bb6f3f0b4
Michael Pollmeier <michael@michaelpollmeier.com>
2021-10-27 17:02:09 +0200

    fix compiler warnings, simplify code

--- ghidra2cpg-tests/src/test/scala/io/joern/ghidra2cpg/querying/x86/LocalNodeTests.scala
+++ ghidra2cpg-tests/src/test/scala/io/joern/ghidra2cpg/querying/x86/LocalNodeTests.scala
@@ -11,12 +11,9 @@ class LocalNodeTests extends GhidraBinToCpgSuite {
   }
 
   "should contain exactly one node with all mandatory fields set" in {
-    cpg.method.name("localNodeTests").local.l.head match {
-      case x =>
-        x.name shouldBe "local_c"
-        x.code shouldBe "[undefined4 local_c@Stack[-0xc]:4]"
-        x.typeFullName shouldBe "undefined4"
-      case _ => fail()
-    }
+    val x = cpg.method.name("localNodeTests").local.l.head
+    x.name shouldBe "local_c"
+    x.code shouldBe "[undefined4 local_c@Stack[-0xc]:4]"
+    x.typeFullName shouldBe "undefined4"
   }
 }
